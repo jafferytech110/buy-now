def5037c41608675ea556c0fead663ff9d251e1e
Sarfaraz Jaffery <sarfarazalijaffery@gmail.com>
2022-10-30 18:15:19 +0100

    completed header and hero section

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import css from './Header.module.css'
+import Logo from '../../assets/logo.png'
+import {CgShoppingBag} from 'react-icons/cg' 
+
+const header = () => {
+  return (
+    <div className={css.container}>
+        <div className={css.logo}>
+            <img src={Logo} alt="buy now logo" />
+            <span>Buy Now</span>
+        </div>
+
+        <div className={css.right}>
+            <div className={css.menu}>
+                <ul className={css.menu}>
+                    <li>Collections</li>
+                    <li>Brands</li>
+                    <li>New</li>
+                    <li>Sales</li>
+                    <li>ENG</li>
+                </ul>
+            </div>
+            <input type="text" className={css.search} placeholder='Search' />
+            <CgShoppingBag className={css.cart} />
+        </div>
+    </div>
+  )
+}
+
+export default header
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import css from './Hero.module.css'
+import Heroimg from '../../assets/hero.png'
+import {RiShoppingBagFill} from 'react-icons/ri'
+import {BsArrowRight} from 'react-icons/bs'
+
+const Hero = () => {
+    return (
+        <div className={css.container}>
+            {/* left side */}
+            <div className={css.h_sides}>
+                <span className={css.text1}>skin protection cream</span>
+                <div className={css.text2}>
+                    <span>Trendy Collections</span>
+                    <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum adipisci maiores magnam.</span>
+                </div>
+            </div>
+            {/* middle side */}
+            <div className={css.wrapper}>
+                <div className={css.blueCircle}></div>
+                <img src={Heroimg} alt="hero" width={600} />
+                <div className={css.cart2}>
+                    <RiShoppingBagFill />
+                    <div className={css.signup}>
+                        <span>Signup Offers</span>
+                        <BsArrowRight />
+                    </div>
+                </div>
+            </div>
+            {/* right side */}
+            <div className={css.h_sides}>
+                <div className={css.traffic}>
+                    <span>1.5m</span>
+                    <span>Monthly Traffic</span>
+                </div>
+                <div className={css.customers}>
+                    <span>100k+</span>
+                    <span>Happy Customers</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
